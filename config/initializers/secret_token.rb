20b317e778bc559260fdc85d002f81773d252698
Andrea <andrea.damico@gmail.com>
2011-06-29 11:03:21 +0200

    initial commit, added fog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SsImmunotox::Application.config.secret_token = '0c6c1b428c89c7a6bed053de915eaea0b13bf07749b33ac451efc9f62ce9b93e476db7189d83ab5ee0b73c6395b94ff7241ef784ffa5c89dd22bd8b49e930878'
