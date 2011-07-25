8ae1f86f9bb9825b551741eb68a186838f2d923c
Andrea <andrea.damico@gmail.com>
2011-07-25 14:16:06 +0200

    adding news engine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110718125135) do
+ActiveRecord::Schema.define(:version => 20110725074436) do
 
   create_table "events", :force => true do |t|
     t.string    "title"
@@ -35,6 +35,30 @@ ActiveRecord::Schema.define(:version => 20110718125135) do
     t.string    "image_uid"
     t.string    "image_ext"
   end
+
+  create_table "news_item_translations", :force => true do |t|
+    t.integer  "news_item_id"
+    t.string   "locale"
+    t.string   "title"
+    t.text     "body"
+    t.string   "external_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "news_item_translations", ["news_item_id"], :name => "index_news_item_translations_on_news_item_id"
+
+  create_table "news_items", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "publish_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "image_id"
+    t.datetime "expiration_date"
+  end
+
+  add_index "news_items", ["id"], :name => "index_news_items_on_id"
 
   create_table "page_part_translations", :force => true do |t|
     t.integer   "page_part_id"
