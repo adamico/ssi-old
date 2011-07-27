ee06dfe2c616d3e2e4ba93a2265edf56c9102a60
Andrea <andrea.damico@gmail.com>
2011-07-27 15:55:31 +0200

    refactoring registrations controller

--- vendor/engines/registrations/app/controllers/registrations_controller.rb
+++ vendor/engines/registrations/app/controllers/registrations_controller.rb
@@ -3,9 +3,6 @@ class RegistrationsController < ApplicationController
   before_filter :find_page, :only => [:create, :new]
   before_filter :find_school
 
-  def thank_you
-    @page = Page.find_by_link_url("/thank_you", :include => [:parts, :slugs])
-  end
 
   def new
     @registration = Registration.new(:school_id => @school.id, :amount => @school.price) if @school
@@ -13,12 +10,11 @@ class RegistrationsController < ApplicationController
 
   def create
     @registration = Registration.new(params[:registration])
+    @registration.ip = request.remote_ip
 
     if @registration.save
       #TODO: add branch for online transfer with sips/atos module
-      RegistrationMailer.registration_confirmation(@registration).deliver
-      @page = Page.find_by_link_url("/thank_you", :include => [:parts, :slugs])
-      render 'thank_you'
+      success
     else
       render 'new'
     end
@@ -32,5 +28,11 @@ class RegistrationsController < ApplicationController
 
   def find_school
     @school = School.try(:next)
+  end
+
+  def success
+    RegistrationMailer.registration_confirmation(@registration).deliver
+    @page = Page.find_by_link_url("/thank_you", :include => [:parts, :slugs])
+    render 'thank_you'
   end
 end
