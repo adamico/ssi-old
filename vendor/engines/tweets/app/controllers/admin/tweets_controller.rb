4c63ee9c2c35f8cd7bdcb74d714b5637ff8d5769
Andrea <andrea.damico@gmail.com>
2011-09-19 11:06:33 +0200

    added notes to add exception catching to tweet api interface

--- vendor/engines/tweets/app/controllers/admin/tweets_controller.rb
+++ vendor/engines/tweets/app/controllers/admin/tweets_controller.rb
@@ -30,18 +30,16 @@ module Admin
 
     private
 
-    #TODO: add a check for api limit before updating and return the response to the user
+    #TODO: catch the exception for api limit before updating and return the response to the user
     def update_twitter_status(tweet_instance)
       status = Twitter.update(tweet_instance.body)
       tweet_instance.update_attribute(:status_id, status.id.to_i)
     end
 
-    #TODO: returns a 404 http status when twitter status_id not found
+    #TODO: catch the 404 http status when status_id not found
     def destroy_twitter_status(tweet_instance)
       twit_status = tweet_instance.status_id.to_i
-      if Twitter.status(twit_status)
-        Twitter.status_destroy(twit_status)
-      end
+      Twitter.status_destroy(twit_status)
     end
   end
 end
