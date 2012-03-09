9055df59c1d1991dc53ce6d9462ce95cfbe01eb6
Andrea D'Amico <andrea.damico@gmail.com>
2012-03-09 11:58:16 +0100

    showing day/month for admin events list

--- vendor/engines/schools/app/models/event.rb
+++ vendor/engines/schools/app/models/event.rb
@@ -8,6 +8,9 @@ class Event < ActiveRecord::Base
   delegate :title, :to => :school, :prefix => true, :allow_nil => true
 
   default_scope order('starts_at ASC')
+  def day_month
+    self.starts_at.strftime("%e/%m")
+  end
   def time
     self.starts_at.strftime("%R")
   end
