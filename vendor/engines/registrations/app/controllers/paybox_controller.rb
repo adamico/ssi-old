061de6b678ae0e4d72ed5ce3428e23a24ae3968d
Andrea D'Amico <andrea.damico@gmail.com>
2012-03-20 10:21:59 +0100

    tweaking payment method pages, adding registration instance to paybox return urls

--- vendor/engines/registrations/app/controllers/paybox_controller.rb
+++ vendor/engines/registrations/app/controllers/paybox_controller.rb
@@ -2,10 +2,10 @@ class PayboxController < ApplicationController
   include Paybox::System::Rails::Integrity
 
   before_filter :find_page, except: :ipn
+  before_filter :find_registration
   before_filter :check_paybox_integrity!, only: :ipn
 
   def ipn
-    @registration = Registration.find(params[:registration])
     if params[:error] == "00000"
       # Yipee, the payment is confirmed!
       # ...
@@ -24,6 +24,10 @@ class PayboxController < ApplicationController
   end
 
   protected
+
+  def find_registration
+    @registration = Registration.find(params[:registration])
+  end
 
   def find_page
     @page = Page.find_by_link_url("/payment")
